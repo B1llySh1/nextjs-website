a6416549418a310f46113b4746de446665cd21ce
BillyShi <lsa84@sfu.ca>
2024-01-02 18:52:27 -0800

    resume update

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import Image from 'next/image'
 export default function Home() {
   return (
     <main>
-      <h2>Billy Shi Home Page</h2>
+      <h2>Home Page</h2>
       <p>Bonjour, my Name is Billy Shi, a fourth year student in Simon Fraser University, majoring in Bsc. Computer Science.</p>
     </main>
   )
